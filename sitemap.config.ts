a4f924f534b2cbfc01496ca31d31a09f68f5142b
JUNSEOK <kd02109@gmail.com>
2023-09-20 17:07:02 +0900

    Fix: package build 과정 수정
    What is this PR?
    - package build 과정 수정
    - robots.txt 생성 과정 추가

--- sitemap.config.ts
+++ sitemap.config.ts
@@ -1,8 +1,8 @@
-import { promises as fs } from 'fs';
+import { promises as fs, writeFileSync } from 'fs';
 import postJson from './.contentlayer/generated/Post/_index.json';
 import { siteConfig } from './src/config';
 
-const SITE_URL = 'https://nextjs-blog-kd02109.vercel.app/';
+const SITE_URL = 'https://nextjs-blog-kd02109.vercel.app';
 export default async function stiempaConfig() {
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:news="http://www.google.com/schemas/sitemap-news/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml" xmlns:mobile="http://www.google.com/schemas/sitemap-mobile/1.0" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1" xmlns:video="http://www.google.com/schemas/sitemap-video/1.1">
@@ -26,5 +26,18 @@ ${postJson
     encoding: 'utf-8',
   });
 }
+const createRobotsTxt = () => {
+  const siteUrl = siteConfig.url;
 
+  const text = `
+User-agent: *
+Allow: /
+Sitemap: ${siteUrl}/sitemap.xml
+Host: ${siteUrl}
+`;
+
+  writeFileSync('public/robots.txt', text.trim(), 'utf-8');
+};
+
+void createRobotsTxt();
 void stiempaConfig();
