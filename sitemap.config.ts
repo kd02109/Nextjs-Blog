33b6db543461b030f18fcbb0ce82b5710231d53e
JUNSEOK <kd02109@gmail.com>
2023-09-20 16:03:21 +0900

    Feat: sitemap 적용하기
    What is this PR?
    - next-sitemap을 인식하지 못하는 듯 하여 직접 사이트맵을 작성하도록 수정했습니다.
    
    Todo

---
+++ sitemap.config.ts
@@ -0,0 +1,30 @@
+import { promises as fs } from 'fs';
+import postJson from './.contentlayer/generated/Post/_index.json';
+import { siteConfig } from './src/config';
+
+const SITE_URL = 'https://nextjs-blog-kd02109.vercel.app/';
+export default async function stiempaConfig() {
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:news="http://www.google.com/schemas/sitemap-news/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml" xmlns:mobile="http://www.google.com/schemas/sitemap-mobile/1.0" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1" xmlns:video="http://www.google.com/schemas/sitemap-video/1.1">
+<url><loc>${SITE_URL}</loc><changefreq>daily</changefreq><priority>0.7</priority></url>
+${siteConfig.menus
+  .map(
+    url =>
+      `<url><loc>${SITE_URL}${url.path}</loc><changefreq>daily</changefreq><priority>0.7</priority></url>`,
+  )
+  .join('\n')}
+${postJson
+  .map(post => {
+    if (post.url.includes('blog'))
+      return `<url><loc>${SITE_URL}blogs/${post.url}</loc><changefreq>daily</changefreq><priority>0.7</priority></url>`;
+    return `<url><loc>${SITE_URL}projects/${post.url}</loc><changefreq>daily</changefreq><priority>0.7</priority></url>`;
+  })
+  .join('\n')}
+</urlset>`;
+
+  await fs.writeFile('public/sitemap.xml', sitemap, {
+    encoding: 'utf-8',
+  });
+}
+
+void stiempaConfig();
