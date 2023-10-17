89c024a74dd33fb5e9f2f8b6e4e28468bab0de5c
kd02109 <57277708+kd02109@users.noreply.github.com>
2023-10-17 15:43:55 +0900

    fix: darkmode 수정 및 글쓰기 (#26)
    What is this PR?
    - browser별 darkmode 색상이 다른 점 보안

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -5,11 +5,20 @@ import { siteConfig } from '@/config';
 import LinkItem from '@/components/LinkItem';
 import DarkSwitch from '@/components/DarkSwitch';
 import Hamburger from '@/components/Hamburger';
+import checkAgent from '@/util/checkAgent';
+
 const Header = () => {
   const pathName = usePathname();
   const [isOpened, setIsOpened] = useState(false);
+  const browserUser = checkAgent();
+
   return (
-    <header className="flex justify-between items-center select-none pt-8 pb-12 sticky z-20 bg-white dark:bg-[#121212] top-0">
+    <header
+      className={`flex justify-between items-center select-none pt-8 pb-12 sticky z-20 bg-white top-0 ${
+        browserUser === 'Mozilla Firefox'
+          ? 'dark:bg-[#1C1B22]'
+          : 'dark:bg-[#121212]'
+      }`}>
       <Hamburger
         isOpened={isOpened}
         setIsOpened={setIsOpened}
