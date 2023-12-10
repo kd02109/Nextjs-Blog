18dfbf11c3960d8e0455238cdd11e96907acb939
JUNSEOK <kd02109@gmail.com>
2023-12-10 13:50:44 +0900

    feat: url 수정
    - url 수정

--- sitemap.config.ts
+++ sitemap.config.ts
@@ -2,7 +2,7 @@ import { promises as fs, writeFileSync } from 'fs';
 import postJson from './.contentlayer/generated/Post/_index.json';
 import { siteConfig } from './src/config';
 
-const SITE_URL = 'https://nextjs-blog-kd02109.vercel.app';
+const SITE_URL = 'https://sonblog.vercel.app';
 export default async function stiempaConfig() {
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:news="http://www.google.com/schemas/sitemap-news/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml" xmlns:mobile="http://www.google.com/schemas/sitemap-mobile/1.0" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1" xmlns:video="http://www.google.com/schemas/sitemap-video/1.1">
